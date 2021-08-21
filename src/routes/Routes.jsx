d4c38cad17ea80bf2d8dc30806c5c31ec278b7b5
Alexandre Menezes <amenezes031@gmail.com>
2021-08-21 16:54:48 -0300

    create routes file

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import { Home } from "../containers/Home";
+
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
