21191cfaf7060c2f56050d0029c15d388f0c05a6
Alexandre Menezes <amenezes031@gmail.com>
2021-08-22 18:11:13 -0300

    endpoint post

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { Home } from "../containers/Home";
+import { Post } from "../containers/Post";
 
 export function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/:subject/:id" exact component={Post} />
       </Switch>
     </BrowserRouter>
   );
